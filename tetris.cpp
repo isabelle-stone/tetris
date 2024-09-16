a14b9f641b1f13fb3d00fce3fec6e3b9cd6ee8cb
Isabelle <ilstone17@gmail.com>
2024-09-16 16:10:24 -0400

    grid bug fixes

--- tetris.cpp
+++ tetris.cpp
@@ -11,11 +11,12 @@ int main() {
     SetTargetFPS(60);
 
     Grid grid = Grid();
+    /* checking box functions
     grid.grid[0][0] = 1;
     grid.grid[3][5] = 4;
     grid.grid[17][8] = 7;
     grid.print();
-
+    */
     // Game Loop:
     while (WindowShouldClose() == false) {
         BeginDrawing();
