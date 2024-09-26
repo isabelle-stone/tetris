8fad52d51539221f4da7240bceb66fe49aa2f389
Isabelle <ilstone17@gmail.com>
2024-09-26 16:00:15 -0400

    L block

---
+++ blocks.cpp
@@ -0,0 +1,10 @@
+#include "block.h"
+#include "position.h"
+
+class LBlock :public Block {
+public:
+    LBlock() {
+        id = 1;
+        cells[0] = {Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)};
+    }
+};
