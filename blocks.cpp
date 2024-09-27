71596956938bc5529636af953599f92c591884ac
Isabelle <ilstone17@gmail.com>
2024-09-27 09:39:40 -0400

    L block

--- blocks.cpp
+++ blocks.cpp
@@ -6,9 +6,8 @@ public:
     LBlock() {
         id = 1;
         cells[0] = {Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)};
-        cells[1] = {Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)};
-        cells[2] = {Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)};
-        cells[3] = {Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)};
-        cells[4] = {Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)};
+        cells[1] = {Position(0, 1), Position(1, 1), Position(2, 1), Position(2, 2)};
+        cells[2] = {Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 0)};
+        cells[3] = {Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)};
     }
 };
