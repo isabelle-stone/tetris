07c8f8861714d3488508099f3cbb710603c9ead7
Isabelle <ilstone17@gmail.com>
2024-09-28 09:09:48 -0400

    J & I  block

--- blocks.cpp
+++ blocks.cpp
@@ -1,6 +1,8 @@
 #include "block.h"
 #include "position.h"
 
+/* PENTOMINOES */
+
 class LBlock :public Block {
 public:
     LBlock() {
@@ -10,4 +12,22 @@ public:
         cells[2] = {Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 0)};
         cells[3] = {Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)};
     }
+};
+
+class JBlock : public Block {
+public:
+    id = 2;
+    cells[0] = {Position(0, 0), Position(1, 0), Position(1, 1), Position(1, 2)};
+    cells[1] = {Position(0, 1), Position(0, 2), Position(1, 1), Position(2, 1)};
+    cells[2] = {Position(1, 0), Position(1, 0), Position(1, 2), Position(2, 2)};
+    cells[3] = {Position(0, 1), Position(1, 1), Position(2, 0), Position(2, 1)};
+};
+
+class IBlock : public Block {
+public:
+    id = 3;
+    cells[0] = {Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)};
+    cells[1] = {Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)};
+    cells[2] = {Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)};
+    cells[3] = {Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)};
 };
