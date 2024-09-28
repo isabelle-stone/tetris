bab352ae3a290b5a83d4cf89d92a3c504b648ddd
Isabelle <ilstone17@gmail.com>
2024-09-28 11:01:06 -0400

    S  block

--- blocks.cpp
+++ blocks.cpp
@@ -1,7 +1,7 @@
 #include "block.h"
 #include "position.h"
 
-/* PENTOMINOES... nvm */
+/* BLOCKS: L, J, I, S, Z, T */
 
 class LBlock :public Block {
 public:
@@ -31,3 +31,23 @@ public:
     cells[2] = {Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)};
     cells[3] = {Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)};
 };
+
+/*
+class OBlock : public Block {
+public:
+    id = 4;
+    cells[0] = {Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)};
+    cells[1] = {Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)};
+    cells[2] = {Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)};
+    cells[3] = {Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)};
+};
+*/
+
+class SBlock : public Block {
+public:
+    id = 5;
+    cells[0] = {Position(0, 1), Position(0, 2), Position(1, 0), Position(1, 1)};
+    cells[1] = {Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 2)};
+    cells[2] = {Position(1, 1), Position(1, 2), Position(2, 0), Position(2, 1)};
+    cells[3] = {Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 1)};
+}
