3144fb6773b6e114d86df8d3addde50231837276
Isabelle <ilstone17@gmail.com>
2024-09-29 08:14:19 -0400

    S  block

--- block.cpp
+++ block.cpp
@@ -7,5 +7,9 @@ Block::Block() {
 }
 
 void Block::Draw() {
+    std::vector<Position> tiles = cells[rotationState];
+    for (Position item: tiles) {
+        DrawRectangle(item.column * cellSize + 1, item.row * cellSize - 1, cellSize - 1, colors[id])
+    }
     
 }
--- tetris.cpp
+++ tetris.cpp
@@ -2,25 +2,23 @@
 #include <raylib.h>
 #include "grid.h"
 #include "colors.h"
-
+#include "block.cpp"
 
 int main() {
     InitWindow(300, 600, "Tetris!");
     SetTargetFPS(60);
 
     Grid grid = Grid();
-    // checking box functions
-    grid.grid[0][0] = 1;
-    grid.grid[3][5] = 4;
-    grid.grid[17][8] = 7;
-    grid.grid[0][1] = 6;
-    grid.print();
+
+    LBlock block = LBlock();
+   
 
     // Game Loop:
     while (WindowShouldClose() == false) {
         BeginDrawing();
         ClearBackground(darkBlue);
         grid.draw();
+        block.draw();
 
         EndDrawing();
     }
