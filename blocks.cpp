ed94269773ce1cb816a6a27dae774c569cbca6b0
Isabelle <ilstone17@gmail.com>
2024-09-30 08:28:09 -0400

    bug fixes

--- blocks.cpp
+++ blocks.cpp
@@ -13,25 +13,30 @@ public:
         cells[3] = {Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)};
     }
 };
-/*
-class JBlock : public Block {
+
+class JBlock :public Block {
 public:
-    id = 2;
-    cells[0] = {Position(0, 0), Position(1, 0), Position(1, 1), Position(1, 2)};
-    cells[1] = {Position(0, 1), Position(0, 2), Position(1, 1), Position(2, 1)};
-    cells[2] = {Position(1, 0), Position(1, 0), Position(1, 2), Position(2, 2)};
-    cells[3] = {Position(0, 1), Position(1, 1), Position(2, 0), Position(2, 1)};
+    JBlock() {
+        id = 2;
+        cells[0] = {Position(0, 0), Position(1, 0), Position(1, 1), Position(1, 2)};
+        cells[1] = {Position(0, 1), Position(0, 2), Position(1, 1), Position(2, 1)};
+        cells[2] = {Position(1, 0), Position(1, 0), Position(1, 2), Position(2, 2)};
+        cells[3] = {Position(0, 1), Position(1, 1), Position(2, 0), Position(2, 1)};
+    }
+
 };
 
-class IBlock : public Block {
+class IBlock :public Block {
 public:
-    id = 3;
-    cells[0] = {Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)};
-    cells[1] = {Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)};
-    cells[2] = {Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)};
-    cells[3] = {Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)};
+    IBlock() {
+        id = 3;
+        cells[0] = {Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)};
+        cells[1] = {Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)};
+        cells[2] = {Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)};
+        cells[3] = {Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)};
+    }
 };
-
+/* 
 class OBlock : public Block {
 public:
     id = 4;
