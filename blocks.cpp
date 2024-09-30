88ea3f5865e7faaef0b08d499a701be2938479e4
Isabelle <ilstone17@gmail.com>
2024-09-30 09:19:33 -0400

    bug fixes

--- blocks.cpp
+++ blocks.cpp
@@ -36,23 +36,28 @@ public:
         cells[3] = {Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)};
     }
 };
-/* 
+
 class OBlock : public Block {
 public:
-    id = 4;
-    cells[0] = {Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)};
-    cells[1] = {Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)};
-    cells[2] = {Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)};
-    cells[3] = {Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)};
+    OBlock() {
+        id = 4;
+        cells[0] = {Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)};
+        cells[1] = {Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)};
+        cells[2] = {Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)};
+        cells[3] = {Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)};
+    }
+
 };
 
 
 class SBlock : public Block {
 public:
-    id = 5;
-    cells[0] = {Position(0, 1), Position(0, 2), Position(1, 0), Position(1, 1)};
-    cells[1] = {Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 2)};
-    cells[2] = {Position(1, 1), Position(1, 2), Position(2, 0), Position(2, 1)};
-    cells[3] = {Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 1)};
+    SBlock() {
+        id = 5;
+        cells[0] = {Position(0, 1), Position(0, 2), Position(1, 0), Position(1, 1)};
+        cells[1] = {Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 2)};
+        cells[2] = {Position(1, 1), Position(1, 2), Position(2, 0), Position(2, 1)};
+        cells[3] = {Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 1)};
+    }
+
 };
-*/
