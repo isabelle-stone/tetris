4bb6b567454fe49e31de0032cb78963045441466
Isabelle <ilstone17@gmail.com>
2024-09-30 12:16:13 -0400

    T block

--- blocks.cpp
+++ blocks.cpp
@@ -49,7 +49,6 @@ public:
 
 };
 
-
 class SBlock : public Block {
 public:
     SBlock() {
@@ -65,12 +64,18 @@ class TBlock : public Block {
 public:
     TBlock() {
         id = 6;
+        cells[0] = {Position(0, 1), Position(1, 0), Position(1, 1), Position(1, 2)};
+        cells[1] = {Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 1)};
+        cells[2] = {Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 1)};
+        cells[3] = {Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 1)};
     }
-;}
+};
 
+/* 
 class ZBlock : public Block {
 public: 
     ZBlock() {
 
     }
 };
+*/
